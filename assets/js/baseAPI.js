fc77eb73415b9cfcfb7b335c4ed595e359c692d0
itheima <itheima@itcast.cn>
2021-08-20 08:44:18 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -15,7 +15,7 @@ $.ajaxPrefilter(function(options) {
     //全局统一挂载 complete 回调函数
     options.complete = function(res) {
         // console.log('执行了complete 回调')
-        // console.log(res)
+        console.log(res)
 
         //在 complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
